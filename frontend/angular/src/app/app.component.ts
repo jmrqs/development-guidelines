68898d1c09b3bed6b8bbeb055f2a61e09d728674
jmrqs <74171400+jmrqs@users.noreply.github.com>
2024-07-11 00:18:47 -0300

    primeng

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,13 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'internet-banking';
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+      this.items = [
+          {
+              label: 'Home',
+              icon: 'pi pi-home'
+          },
+          {
+              label: 'Features',
+              icon: 'pi pi-star'
+          },
+          {
+              label: 'Projects',
+              icon: 'pi pi-search',
+              items: [
+                  {
+                      label: 'Core',
+                      icon: 'pi pi-bolt',
+                      shortcut: '⌘+S'
+                  },
+                  {
+                      label: 'Blocks',
+                      icon: 'pi pi-server',
+                      shortcut: '⌘+B'
+                  },
+                  {
+                      label: 'UI Kit',
+                      icon: 'pi pi-pencil',
+                      shortcut: '⌘+U'
+                  },
+                  {
+                      separator: true
+                  },
+                  {
+                      label: 'Templates',
+                      icon: 'pi pi-palette',
+                      items: [
+                          {
+                              label: 'Apollo',
+                              icon: 'pi pi-palette',
+                              badge: '2'
+                          },
+                          {
+                              label: 'Ultima',
+                              icon: 'pi pi-palette',
+                              badge: '3'
+                          }
+                      ]
+                  }
+              ]
+          },
+          {
+              label: 'Contact',
+              icon: 'pi pi-envelope',
+              badge: '3'
+          }
+      ];
+  }
 }
